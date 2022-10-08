39982035331ba0e0c00f4766b81dc2f0c6ff7685
Gubska Nastya <97690198+ngubskaja@users.noreply.github.com>
2022-10-08 23:45:40 +0300

    add

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -31,8 +31,8 @@ paletteContainer.addEventListener('click', onPaletteContainerClick);
 if(!evt.target.classList.contains('gallery__image')){
     return;
 }
-const lightbox = new SimpleLightbox('.gallery a', {animationSlide:true, captionsData:"alt", captionDelay:250});
-// new SimpleLightbox('.gallery a', {captionsData:"alt", captionDelay:250});
+const lightbox = new SimpleLightbox('.gallery a', { captionsData:"alt", captionDelay:250});
+// new SimpleLightbox('.gallery a', {animationSlide:true, captionsData:"alt", captionDelay:250});
 
   }
 
