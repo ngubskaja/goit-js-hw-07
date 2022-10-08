9f7b8cd7996f3cf65ac326feb0279a8d91760ed2
Gubska Nastya <97690198+ngubskaja@users.noreply.github.com>
2022-10-08 22:38:30 +0300

    add

---
+++ js/02-lightbox.js
@@ -0,0 +1,41 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+
+const paletteContainer = document.querySelector('.gallery');
+const cardsMarkUp = createGalleryMarkup(galleryItems);
+
+paletteContainer.insertAdjacentHTML('beforeend', cardsMarkUp);
+
+console.log(createGalleryMarkup(galleryItems));
+
+paletteContainer.addEventListener('click', onPaletteContainerClick);
+
+
+ function createGalleryMarkup(galleryItems) {
+    return galleryItems
+      .map(({ preview, original, description }) => {
+        return `
+        <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+        `;
+      })
+      .join('');
+      
+  }
+
+  function onPaletteContainerClick (evt){
+    evt.preventDefault();
+
+if(!evt.target.classList.contains('gallery__image')){
+    return;
+}
+const lightbox = new SimpleLightbox('.gallery a', {animationSpeed: 250});
+new SimpleLightbox('.gallery a', {captionsData:"alt", captionDelay:250});
+
+  }
+
+  
